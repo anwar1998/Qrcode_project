47a88313714b3a7817a6d547da6318f89789be38
unknown <anouar.gmili@usmba.ac.ma>
2023-05-20 15:54:00 +0100

    delete Qrcode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     Home
                 </a>
                 <a class="navbar-brand" href="{{ url('/qcodes') }}">
--- resources/views/qrcode_page.blade.php
+++ resources/views/qrcode_page.blade.php
@@ -8,131 +8,9 @@
                     <div class="card-body">
                         <!-- Button trigger modal -->
                         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            ADD
+                            Génère un nouveau Qr Code
                         </button>
                         <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                            aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <form action="qcodes" method="POST">
-                                        @csrf
-                                        <div class="modal-body">
-                                            <div class="form-group">
-                                                <label for="nom">Nom :</label>
-                                                <input type="text" class="form-control" id="nom" name="name"
-                                                    required>
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Type :</label>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="type"
-                                                        class="chngRadio" id="valeur1" value="number" required>
-                                                    <label class="form-check-label" for="valeur1">
-                                                        Téle
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="type"
-                                                        class="chngRadio" id="valeur2" value="site">
-                                                    <label class="form-check-label" for="valeur2">
-                                                        Site web
-                                                    </label>
-                                                </div>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="radio" name="type"
-                                                        class="chngRadio" id="valeur3" value="page">
-                                                    <label class="form-check-label" for="valeur3">
-                                                        Page facebook
-                                                    </label>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="valeur">Valeur :</label>
-                                                <input type="text" class="form-control" id="qrcode_value"
-                                                    name="qrcode_value" required>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="description">Description :</label>
-                                                <textarea class="form-control" id="description"></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-primary">Save changes</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                        {{--  --}}
-                        <div class="modal fade" id="exampleModalDelete" tabindex="-1" aria-labelledby="exampleModalLabel"
-                            aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Supprimer Qrcode</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <form action="qcodes" method="POST" id="form_for_delete">
-                                        @csrf
-                                        @method('DELETE')
-                                        <div class="row mx-auto justify-content-center align-items-center flex-column ">
-                                            <div>
-                                                <input type="text" name="id_qrcode_delete" id="id_qrcode_delete">
-                                                Voulez vous_vraiment supprimer ce Qrcode?
-                                                {{-- <div id="qrcode"></div> --}}
-                                                {{-- <div class="card text-white bg-info ">
-                                            <div class="card-body ">
-                                                <h5 class="card-title" id="type_qrcode"></h5>
-                                                <p class="card-text" id=value_qrcode>
-                                                </p>
-                                            </div>
-                                        </div> --}}
-                                            </div>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">Non</button>
-                                            <button type="submit" class="btn btn-danger">Supprimer</button>
-                                        </div>
-                                    </form>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="modal fade" id="exampleModalDetail" tabindex="-1"
-                            aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Qrcode</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <div class="row mx-auto justify-content-center align-items-center flex-column ">
-                                        <div>
-                                            <div id="qrcode"></div>
-                                            <div class="card text-white bg-info ">
-                                                <div class="card-body ">
-                                                    <h5 class="card-title" id="type_qrcode"></h5>
-                                                    <p class="card-text" id=value_qrcode>
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
                         <table class="table">
                             <thead class="thead-dark">
@@ -171,12 +49,114 @@
             </div>
         </div>
     </div>
-    <style>
-        .pagination .img,
-        svg {
-            width: 15px
-        }
-    </style>
+
+    {{-- modal --}}
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Nouveau Qr Code</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="qcodes" method="POST">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="nom">Titre :</label>
+                            <input type="text" class="form-control" id="nom" name="name" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Type :</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="type" class="chngRadio"
+                                    id="valeur1" value="number" required>
+                                <label class="form-check-label" for="valeur1">
+                                    Téle
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="type" class="chngRadio"
+                                    id="valeur2" value="site">
+                                <label class="form-check-label" for="valeur2">
+                                    Site web
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="type" class="chngRadio"
+                                    id="valeur3" value="page">
+                                <label class="form-check-label" for="valeur3">
+                                    Page facebook/instagram
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="valeur">Valeur :</label>
+                            <input type="text" class="form-control" id="qrcode_value" name="qrcode_value" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description :</label>
+                            <textarea class="form-control" id="description" name="description"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                        <button type="submit" class="btn btn-primary">Génerer</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="exampleModalDelete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Supprimer Qr code</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="qcodes" method="POST" id="form_for_delete">
+                    @csrf
+                    @method('DELETE')
+                    <div class="row mx-auto justify-content-center align-items-center flex-column ">
+                        <div>
+                            Voulez vous vraiment supprimer ce Qr Code?
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Non</button>
+                        <button type="submit" class="btn btn-danger">Supprimer</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="exampleModalDetail" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Qr code</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="row mx-auto justify-content-center align-items-center flex-column ">
+                    <div class="row mx-auto justify-content-center align-items-center flex-column ">
+                        <div id="qrcode" class="mt-4"></div>
+                        <div class="card text-white bg-info mt-4">
+                            <div class="card-body ">
+                                <h5 class="card-title" id="type_qrcode"></h5>
+                                <p class="card-text" id=value_qrcode>
+                                </p>
+                            </div>
+                        </div>
+                        {{-- <button>Télécharger</button> --}}
+                        <button id="downloadButton" class="btn btn-success w-75 mt-4 mb-4" onclick="downloadQRCode()">
+                            Télécharger</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <script>
         $(document).ready(function() {
             // Listen for change event on radio inputs
@@ -196,23 +176,45 @@
                 }
             });
         });
+        var qrDiv = document.getElementById("qrcode");
 
         function Delete_Qrcode(Qrcode_object) {
-            document.getElementById('id_qrcode_delete').value = Qrcode_object.id
             var form = document.getElementById('form_for_delete');
-            form.action = "qcodes/"+Qrcode_object.id;
+            form.action = "qcodes/" + Qrcode_object.id;
         }
 
         function Charger_qrcode(Qrcode_Obj) {
-            document.getElementById("qrcode").innerHTML = ""
+            var qr;
+            qrDiv.innerHTML = ""
             document.getElementById("type_qrcode").innerHTML = "Type :" + Qrcode_Obj.type
             document.getElementById("value_qrcode").innerHTML = Qrcode_Obj.qrcode_value
-            var qr = {
-                text: Qrcode_Obj.qrcode_value,
-                width: 300,
-                height: 300
-            };
-            var qrcode = new QRCode(document.getElementById("qrcode"), qr);
+            if (Qrcode_Obj.type == "NUMBER") {
+                qr = {
+                    text: "tel:" + Qrcode_Obj.qrcode_value,
+                    width: 300,
+                    height: 300
+                }
+            } else {
+                qr = {
+                    text: Qrcode_Obj.qrcode_value,
+                    width: 300,
+                    height: 300
+                }
+            }
+            var qrcode = new QRCode(qrDiv, qr);
+        }
+
+        function downloadQRCode() {
+            var link = document.createElement("a");
+            link.href = qrDiv.querySelector("img").src;
+            link.download = "qrcode.png";
+            link.click();
         }
     </script>
+    <style>
+        .pagination .img,
+        svg {
+            width: 15px
+        }
+    </style>
 @endsection
